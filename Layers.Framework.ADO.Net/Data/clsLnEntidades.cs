3603e8057d23c01dd29fc6c513783b6f0b5bb746
bachiito <riveraabraham332@gmail.com>
2022-04-25 07:56:36 -0400

    clsLnEntidades arreglado

--- Layers.Framework.ADO.Net/Data/clsLnEntidades.cs
+++ Layers.Framework.ADO.Net/Data/clsLnEntidades.cs
@@ -6,7 +6,7 @@ using Layers.Framework.ADO.Net.Data;
 
 public class clsLnEntidades
 {
-    public void Cargar(ref clsBeEntidades oBeEntidades, ref DataRow dr)
+    public void Cargar(ref clsBeEntidades oBeEntidades, DataRow dr)
     {
         try
         {
@@ -45,40 +45,40 @@ public class clsLnEntidades
         try
         {
             int rowsAffected = 0;
-            string sp = "SpEntidadesInsertar";
+            string storedProcedure = "SpEntidadesInsertar";
 
             SqlConnection sqlConnection = new SqlConnection(Datos.DBConnection);
-            SqlCommand sqlCommand = new SqlCommand(sp, sqlConnection);
+            SqlCommand sqlCommand = new SqlCommand(storedProcedure, sqlConnection);
             
             sqlConnection.Open();
 
-            sqlCommand.Parameters.Add(new SqlClient.SqlParameter("@IDENTIDAD", oBeEntidades.IdEntidad));
-            sqlCommand.Parameters("@IDENTIDAD").Direction = ParameterDirection.Output;
-            sqlCommand.Parameters.Add(new SqlClient.SqlParameter("@DESCRIPCION", oBeEntidades.Descripcion));
-            sqlCommand.Parameters.Add(new SqlClient.SqlParameter("@DIRECCION", oBeEntidades.Direccion));
-            sqlCommand.Parameters.Add(new SqlClient.SqlParameter("@LOCALIDAD", oBeEntidades.Localidad));
-            sqlCommand.Parameters.Add(new SqlClient.SqlParameter("@TIPOENTIDAD", oBeEntidades.TipoEntidad));
-            sqlCommand.Parameters.Add(new SqlClient.SqlParameter("@TIPODOCUMENTO", oBeEntidades.TipoDocumento));
-            sqlCommand.Parameters.Add(new SqlClient.SqlParameter("@NUMERODOCUMENTO", oBeEntidades.NumeroDocumento));
-            sqlCommand.Parameters.Add(new SqlClient.SqlParameter("@TELEFONO", oBeEntidades.Telefono));
-            sqlCommand.Parameters.Add(new SqlClient.SqlParameter("@URLPAGINAWEB", oBeEntidades.URLPaginaWeb));
-            sqlCommand.Parameters.Add(new SqlClient.SqlParameter("@URLFACEBOOK", oBeEntidades.URLFacebook));
-            sqlCommand.Parameters.Add(new SqlClient.SqlParameter("@URLINSTAGRAM", oBeEntidades.URLInstagram));
-            sqlCommand.Parameters.Add(new SqlClient.SqlParameter("@URLTWITTER", oBeEntidades.URLTwitter));
-            sqlCommand.Parameters.Add(new SqlClient.SqlParameter("@URLTIKTOK", oBeEntidades.URLTiktok));
-            sqlCommand.Parameters.Add(new SqlClient.SqlParameter("@IDGRUPOENTIDAD", oBeEntidades.IdGrupoEntidad));
-            sqlCommand.Parameters.Add(new SqlClient.SqlParameter("@IDTIPOENTIDAD", oBeEntidades.IdTipoEntidad));
-            sqlCommand.Parameters.Add(new SqlClient.SqlParameter("@LIMITECREDITO", oBeEntidades.LimiteCredito));
-            sqlCommand.Parameters.Add(new SqlClient.SqlParameter("@USERNAMEENTIDAD", oBeEntidades.UserNameEntidad));
-            sqlCommand.Parameters.Add(new SqlClient.SqlParameter("@PASSWORDENTIDAD", oBeEntidades.PasswordEntidad));
-            sqlCommand.Parameters.Add(new SqlClient.SqlParameter("@ROLUSERENTIDAD", oBeEntidades.RolUserEntidad));
-            sqlCommand.Parameters.Add(new SqlClient.SqlParameter("@COMENTARIO", oBeEntidades.Comentario));
-            sqlCommand.Parameters.Add(new SqlClient.SqlParameter("@STATUS", oBeEntidades.Status));
-            sqlCommand.Parameters.Add(new SqlClient.SqlParameter("@NOELIMINABLE", oBeEntidades.NoEliminable));
-            sqlCommand.Parameters.Add(new SqlClient.SqlParameter("@FECHAREGISTRO", oBeEntidades.FechaRegistro));
+            sqlCommand.Parameters.Add(new SqlParameter("@IDENTIDAD", oBeEntidades.IdEntidad));
+            sqlCommand.Parameters["@IDENTIDAD"].Direction = ParameterDirection.Output;
+            sqlCommand.Parameters.Add(new SqlParameter("@DESCRIPCION", oBeEntidades.Descripcion));
+            sqlCommand.Parameters.Add(new SqlParameter("@DIRECCION", oBeEntidades.Direccion));
+            sqlCommand.Parameters.Add(new SqlParameter("@LOCALIDAD", oBeEntidades.Localidad));
+            sqlCommand.Parameters.Add(new SqlParameter("@TIPOENTIDAD", oBeEntidades.TipoEntidad));
+            sqlCommand.Parameters.Add(new SqlParameter("@TIPODOCUMENTO", oBeEntidades.TipoDocumento));
+            sqlCommand.Parameters.Add(new SqlParameter("@NUMERODOCUMENTO", oBeEntidades.NumeroDocumento));
+            sqlCommand.Parameters.Add(new SqlParameter("@TELEFONO", oBeEntidades.Telefono));
+            sqlCommand.Parameters.Add(new SqlParameter("@URLPAGINAWEB", oBeEntidades.URLPaginaWeb));
+            sqlCommand.Parameters.Add(new SqlParameter("@URLFACEBOOK", oBeEntidades.URLFacebook));
+            sqlCommand.Parameters.Add(new SqlParameter("@URLINSTAGRAM", oBeEntidades.URLInstagram));
+            sqlCommand.Parameters.Add(new SqlParameter("@URLTWITTER", oBeEntidades.URLTwitter));
+            sqlCommand.Parameters.Add(new SqlParameter("@URLTIKTOK", oBeEntidades.URLTiktok));
+            sqlCommand.Parameters.Add(new SqlParameter("@IDGRUPOENTIDAD", oBeEntidades.IdGrupoEntidad));
+            sqlCommand.Parameters.Add(new SqlParameter("@IDTIPOENTIDAD", oBeEntidades.IdTipoEntidad));
+            sqlCommand.Parameters.Add(new SqlParameter("@LIMITECREDITO", oBeEntidades.LimiteCredito));
+            sqlCommand.Parameters.Add(new SqlParameter("@USERNAMEENTIDAD", oBeEntidades.UserNameEntidad));
+            sqlCommand.Parameters.Add(new SqlParameter("@PASSWORDENTIDAD", oBeEntidades.PasswordEntidad));
+            sqlCommand.Parameters.Add(new SqlParameter("@ROLUSERENTIDAD", oBeEntidades.RolUserEntidad));
+            sqlCommand.Parameters.Add(new SqlParameter("@COMENTARIO", oBeEntidades.Comentario));
+            sqlCommand.Parameters.Add(new SqlParameter("@STATUS", oBeEntidades.Status));
+            sqlCommand.Parameters.Add(new SqlParameter("@NOELIMINABLE", oBeEntidades.NoEliminable));
+            sqlCommand.Parameters.Add(new SqlParameter("@FECHAREGISTRO", oBeEntidades.FechaRegistro));
 
             rowsAffected = sqlCommand.ExecuteNonQuery();
-            oBeEntidades.IdEntidad = Convert.ToInt32(sqlCommand.Parameters["@IDENTIDAD"].Value);
+            oBeEntidades.IdEntidad = Convert.ToInt32(sqlCommand.Parameters["@IDENTIDAD"].Value.ToString());
 
             return rowsAffected;
         }
@@ -92,42 +92,41 @@ public class clsLnEntidades
     {
         try
         {
-            string sp = "SpEntidadesActualizar";
-
-            SqlConnection cnn = new SqlConnection(oConexion.ConexionLocal);
-            SqlClient.SqlCommand cmd = new SqlClient.SqlCommand(sp, cnn);
-
             int rowsAffected = 0;
-            cmd.Open();
+            string storedProcedure = "SpEntidadesActualizar";
 
-            cmd.Parameters.Add(new SqlClient.SqlParameter("@IDENTIDAD", oBeEntidades.IdEntidad));
-            cmd.Parameters.Add(new SqlClient.SqlParameter("@DESCRIPCION", oBeEntidades.Descripcion));
-            cmd.Parameters.Add(new SqlClient.SqlParameter("@DIRECCION", oBeEntidades.Direccion));
-            cmd.Parameters.Add(new SqlClient.SqlParameter("@LOCALIDAD", oBeEntidades.Localidad));
-            cmd.Parameters.Add(new SqlClient.SqlParameter("@TIPOENTIDAD", oBeEntidades.TipoEntidad));
-            cmd.Parameters.Add(new SqlClient.SqlParameter("@TIPODOCUMENTO", oBeEntidades.TipoDocumento));
-            cmd.Parameters.Add(new SqlClient.SqlParameter("@NUMERODOCUMENTO", oBeEntidades.NumeroDocumento));
-            cmd.Parameters.Add(new SqlClient.SqlParameter("@TELEFONO", oBeEntidades.Telefono));
-            cmd.Parameters.Add(new SqlClient.SqlParameter("@URLPAGINAWEB", oBeEntidades.URLPaginaWeb));
-            cmd.Parameters.Add(new SqlClient.SqlParameter("@URLFACEBOOK", oBeEntidades.URLFacebook));
-            cmd.Parameters.Add(new SqlClient.SqlParameter("@URLINSTAGRAM", oBeEntidades.URLInstagram));
-            cmd.Parameters.Add(new SqlClient.SqlParameter("@URLTWITTER", oBeEntidades.URLTwitter));
-            cmd.Parameters.Add(new SqlClient.SqlParameter("@URLTIKTOK", oBeEntidades.URLTiktok));
-            cmd.Parameters.Add(new SqlClient.SqlParameter("@IDGRUPOENTIDAD", oBeEntidades.IdGrupoEntidad));
-            cmd.Parameters.Add(new SqlClient.SqlParameter("@IDTIPOENTIDAD", oBeEntidades.IdTipoEntidad));
-            cmd.Parameters.Add(new SqlClient.SqlParameter("@LIMITECREDITO", oBeEntidades.LimiteCredito));
-            cmd.Parameters.Add(new SqlClient.SqlParameter("@USERNAMEENTIDAD", oBeEntidades.UserNameEntidad));
-            cmd.Parameters.Add(new SqlClient.SqlParameter("@PASSWORDENTIDAD", oBeEntidades.PasswordEntidad));
-            cmd.Parameters.Add(new SqlClient.SqlParameter("@ROLUSERENTIDAD", oBeEntidades.RolUserEntidad));
-            cmd.Parameters.Add(new SqlClient.SqlParameter("@COMENTARIO", oBeEntidades.Comentario));
-            cmd.Parameters.Add(new SqlClient.SqlParameter("@STATUS", oBeEntidades.Status));
-            cmd.Parameters.Add(new SqlClient.SqlParameter("@NOELIMINABLE", oBeEntidades.NoEliminable));
-            cmd.Parameters.Add(new SqlClient.SqlParameter("@FECHAREGISTRO", oBeEntidades.FechaRegistro));
+            SqlConnection sqlConnection = new SqlConnection(Datos.DBConnection);
+            SqlCommand sqlCommand = new SqlCommand(storedProcedure, sqlConnection);
 
-            rowsAffected = cmd.ExecuteNonQuery();
+            sqlConnection.Open();
+
+            sqlCommand.Parameters.Add(new SqlParameter("@IDENTIDAD", oBeEntidades.IdEntidad));
+            sqlCommand.Parameters.Add(new SqlParameter("@DESCRIPCION", oBeEntidades.Descripcion));
+            sqlCommand.Parameters.Add(new SqlParameter("@DIRECCION", oBeEntidades.Direccion));
+            sqlCommand.Parameters.Add(new SqlParameter("@LOCALIDAD", oBeEntidades.Localidad));
+            sqlCommand.Parameters.Add(new SqlParameter("@TIPOENTIDAD", oBeEntidades.TipoEntidad));
+            sqlCommand.Parameters.Add(new SqlParameter("@TIPODOCUMENTO", oBeEntidades.TipoDocumento));
+            sqlCommand.Parameters.Add(new SqlParameter("@NUMERODOCUMENTO", oBeEntidades.NumeroDocumento));
+            sqlCommand.Parameters.Add(new SqlParameter("@TELEFONO", oBeEntidades.Telefono));
+            sqlCommand.Parameters.Add(new SqlParameter("@URLPAGINAWEB", oBeEntidades.URLPaginaWeb));
+            sqlCommand.Parameters.Add(new SqlParameter("@URLFACEBOOK", oBeEntidades.URLFacebook));
+            sqlCommand.Parameters.Add(new SqlParameter("@URLINSTAGRAM", oBeEntidades.URLInstagram));
+            sqlCommand.Parameters.Add(new SqlParameter("@URLTWITTER", oBeEntidades.URLTwitter));
+            sqlCommand.Parameters.Add(new SqlParameter("@URLTIKTOK", oBeEntidades.URLTiktok));
+            sqlCommand.Parameters.Add(new SqlParameter("@IDGRUPOENTIDAD", oBeEntidades.IdGrupoEntidad));
+            sqlCommand.Parameters.Add(new SqlParameter("@IDTIPOENTIDAD", oBeEntidades.IdTipoEntidad));
+            sqlCommand.Parameters.Add(new SqlParameter("@LIMITECREDITO", oBeEntidades.LimiteCredito));
+            sqlCommand.Parameters.Add(new SqlParameter("@USERNAMEENTIDAD", oBeEntidades.UserNameEntidad));
+            sqlCommand.Parameters.Add(new SqlParameter("@PASSWORDENTIDAD", oBeEntidades.PasswordEntidad));
+            sqlCommand.Parameters.Add(new SqlParameter("@ROLUSERENTIDAD", oBeEntidades.RolUserEntidad));
+            sqlCommand.Parameters.Add(new SqlParameter("@COMENTARIO", oBeEntidades.Comentario));
+            sqlCommand.Parameters.Add(new SqlParameter("@STATUS", oBeEntidades.Status));
+            sqlCommand.Parameters.Add(new SqlParameter("@NOELIMINABLE", oBeEntidades.NoEliminable));
+            sqlCommand.Parameters.Add(new SqlParameter("@FECHAREGISTRO", oBeEntidades.FechaRegistro));
+
+            rowsAffected = sqlCommand.ExecuteNonQuery();
 
             return rowsAffected;
-
         }
         catch (Exception ex)
         {
@@ -139,20 +138,17 @@ public class clsLnEntidades
     {
         try
         {
-            string sp = "SpEntidadesEliminar";
-
-            SqlConnection cnn = new SqlConnection(oConexion.ConexionLocal);
-            SqlClient.SqlCommand cmd = new SqlClient.SqlCommand(sp, cnn);
-
             int rowsAffected = 0;
-            cmd.Open();
+            string storedProcedure = "SpEntidadesEliminar";
 
-            cmd.Parameters.Add(new SqlClient.SqlParameter("@IDENTIDAD", oBeEntidades.IdEntidad));
+            SqlConnection sqlConnection = new SqlConnection(Datos.DBConnection);
+            SqlCommand sqlCommand = new SqlCommand(storedProcedure, sqlConnection);
 
-            rowsAffected = cmd.ExecuteNonQuery();
+            sqlConnection.Open();
+            sqlCommand.Parameters.Add(new SqlParameter("@IDENTIDAD", oBeEntidades.IdEntidad));
+            rowsAffected = sqlCommand.ExecuteNonQuery();
 
             return rowsAffected;
-
         }
         catch (Exception ex)
         {
@@ -164,19 +160,18 @@ public class clsLnEntidades
     {
         try
         {
-            string sp = "SpEntidadesListar";
+            string storedProcedure = "SpEntidadesListar";
 
-            SqlConnection cnn = new SqlConnection(oConexion.ConexionLocal);
-            SqlCommand cmd = new SqlCommand(sp, cnn);
-            cmd.CommandType = CommandType.StoredProcedure;
+            DataTable dataTable = new DataTable();
+            SqlConnection sqlConnection = new SqlConnection(Datos.DBConnection);
+            
+            SqlCommand sqlCommand = new SqlCommand(storedProcedure, sqlConnection);
+            sqlCommand.CommandType = CommandType.StoredProcedure;
 
-            SqlDataAdapter dad = new SqlDataAdapter(cmd);
+            SqlDataAdapter sqlAdapter = new SqlDataAdapter(sqlCommand);
+            sqlAdapter.Fill(dataTable);
 
-            DataTable dt = new DataTable();
-            dad.Fill(dt);
-
-            return dt;
-
+            return dataTable;
         }
         catch (Exception ex)
         {
@@ -188,21 +183,21 @@ public class clsLnEntidades
     {
         try
         {
-            string sp = "SpEntidadesObtener";
+            string storedProcedure = "SpEntidadesObtener";
 
-            SqlConnection cnn = new SqlConnection(oConexion.ConexionLocal);
-            SqlCommand cmd = new SqlCommand(sp, cnn);
-            cmd.CommandType = CommandType.StoredProcedure;
+            DataTable dataTable = new DataTable();
+            SqlConnection sqlConnection = new SqlConnection(Datos.DBConnection);
+            
+            SqlCommand sqlCommand = new SqlCommand(storedProcedure, sqlConnection);
+            sqlCommand.CommandType = CommandType.StoredProcedure;
 
-            SqlDataAdapter dad = new SqlDataAdapter(cmd);
-            dad.SelectCommand.Parameters.Add(new SqlClient.SqlParameter("@IDENTIDAD", oBeEntidades.IdEntidad));
+            SqlDataAdapter sqlAdapter = new SqlDataAdapter(sqlCommand);
+            sqlAdapter.SelectCommand.Parameters.Add(new SqlParameter("@IDENTIDAD", oBeEntidades.IdEntidad));
+            sqlAdapter.Fill(dataTable);
 
-            DataTable dt = new DataTable();
-            dad.Fill(dt);
-
-            if ((dt.Rows.Count == 1))
+            if ((dataTable.Rows.Count == 1))
             {
-                Cargar(oBeEntidades, dt.Rows(0))
+                Cargar(ref oBeEntidades, dataTable.Rows[0]);
 
             }
             else
@@ -211,147 +206,10 @@ public class clsLnEntidades
             }
 
             return true;
-
         }
         catch (Exception ex)
         {
             throw ex;
         }
     }
-
-    public bool Primero(ref clsBeEntidades oBeEntidades)
-    {
-        try
-
-            string sp = "SpEntidadesPrimero";
-
-        SqlConnection cnn = new SqlConnection(oConexion.ConexionLocal);
-        SqlCommand cmd = new SqlCommand(sp, cnn);
-        cmd.CommandType = CommandType.StoredProcedure;
-
-        SqlDataAdapter dad = new SqlDataAdapter(cmd);
-
-        DataTable dt = new DataTable();
-        dad.Fill(dt);
-
-        if ((dt.Rows.Count == 1))
-        {
-            Cargar(oBeEntidades, dt.Rows[0]);
-        }
-        else
-        {
-            throw new Exception("No se pudo obtener el primer registro");
-        }
-
-        return true;
-    }
-		catch (Exception ex) {
-			throw ex;
-		}
-	}
-
-	public bool Primero(ref clsBeEntidades oBeEntidades)
-{
-    try
-    {
-        string sp = "SpEntidadesUltimo";
-
-        SqlConnection cnn = new SqlConnection(oConexion.ConexionLocal);
-        SqlCommand cmd = new SqlCommand(sp, cnn);
-        cmd.CommandType = CommandType.StoredProcedure;
-
-        SqlDataAdapter dad = new SqlDataAdapter(cmd);
-
-        DataTable dt = new DataTable();
-        dad.Fill(dt);
-
-        if ((dt.Rows.Count == 1))
-        {
-            Cargar(oBeEntidades, dt.Rows[0]);
-        }
-        else
-        {
-            throw new Exception("No se pudo obtener el primer registro");
-        }
-
-        return true;
-    }
-    catch (Exception ex)
-    {
-        throw ex;
-    }
-}
-
-public bool Anterior(ref clsBeEntidades oBeEntidades)
-{
-    try
-
-            string sp = "SpEntidadesAnterior";
-
-    SqlConnection cnn = new SqlConnection(oConexion.ConexionLocal);
-    SqlCommand cmd = new SqlCommand(sp, cnn);
-    cmd.CommandType = CommandType.StoredProcedure;
-
-    SqlDataAdapter dad = new SqlDataAdapter(cmd);
-    dad.SelectCommand.Parameters.Add(new SqlClient.SqlParameter("@IDENTIDAD", oBeEntidades.IdEntidad));
-
-    DataTable dt = new DataTable();
-    dad.Fill(dt);
-
-    if ((dt.Rows.Count == 1))
-    {
-        Cargar(oBeEntidades, dt.Rows[0]);
-    }
-    else
-    {
-        throw new Exception("No se pudo obtener el anterior registro");
-        End If
-
-            }
-
-    return true;
-}
-
-        catch (Exception ex)
-{
-    throw ex;
-}
-	}
-
-	public bool Siguiente(ref clsBeEntidades oBeEntidades)
-{
-    try
-
-            string sp = "SpEntidadesSiguiente";
-
-    SqlConnection cnn = new SqlConnection(oConexion.ConexionLocal);
-    SqlCommand cmd = new SqlCommand(sp, cnn);
-    cmd.CommandType = CommandType.StoredProcedure;
-
-    SqlDataAdapter dad = new SqlDataAdapter(cmd);
-    dad.SelectCommand.Parameters.Add(new SqlClient.SqlParameter("@IDENTIDAD", oBeEntidades.IdEntidad));
-
-    DataTable dt = new DataTable();
-    dad.Fill(dt);
-
-    if ((dt.Rows.Count == 1))
-    {
-        Cargar(oBeEntidades, dt.Rows[0]);
-    }
-    else
-    {
-        throw new Exception("No se pudo obtener el siguiente registro");
-        End If
-
-            }
-
-    return true;
-}
-
-        catch (Exception ex)
-{
-    throw ex;
-}
-	}
-
 }
