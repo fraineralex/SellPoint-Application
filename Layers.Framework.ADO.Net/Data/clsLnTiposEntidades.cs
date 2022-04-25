4aa737b09cab68ee52aaab912c04b9d934a4a91e
bachiito <riveraabraham332@gmail.com>
2022-04-25 08:31:34 -0400

    clsLnTiposEntidades arreglado

--- Layers.Framework.ADO.Net/Data/clsLnTiposEntidades.cs
+++ Layers.Framework.ADO.Net/Data/clsLnTiposEntidades.cs
@@ -1,274 +1,167 @@
-using BE;
-using System.Data.SqlClient;
+using System;
 using System.Data;
+using System.Data.SqlClient;
+using Layers.Framework.ADO.Net;
+using Layers.Framework.ADO.Net.Data;
 
-public class clsLnTiposEntidades {
+public class clsLnTiposEntidades
+{
 
-	private ConnectionManager oConexion = null;
+    public void Cargar(ref clsBeTiposEntidades oBeTiposEntidades, DataRow dr)
+    {
+        try
+        {
+            oBeTiposEntidades.IdTipoEntidad = Convert.ToInt32(dr["IdTipoEntidad"].ToString());
+            oBeTiposEntidades.Descripcion = string.IsNullOrEmpty(dr["Descripcion"].ToString()) == true ? "" : dr["Descripcion"].ToString();
+            oBeTiposEntidades.IdGrupoEntidad = Convert.ToInt32(dr["IdGrupoEntidad"].ToString());
+            oBeTiposEntidades.Comentario = string.IsNullOrEmpty(dr["Comentario"].ToString()) == true ? "" : dr["Comentario"].ToString();
+            oBeTiposEntidades.Status = string.IsNullOrEmpty(dr["Status"].ToString()) == true ? "" : dr["Status"].ToString();
+            oBeTiposEntidades.NoEliminable = Convert.ToBoolean(dr["IdEntidad"].ToString());
+            oBeTiposEntidades.FechaRegistro = Convert.ToDateTime(dr["IdEntidad"].ToString());
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+    }
 
-	 public clsLnTiposEntidades(ConnectionManager oConexion) {
-		try {
-			this.oConexion = oConexion;
-		}
-		catch (Exception ex) {
-			throw ex;
-		}
-	}
+    public int Insertar(ref clsBeTiposEntidades oBeTiposEntidades)
+    {
+        try
+        {
+            int rowsAffected = 0;
+            string storedProcedure = "SpTiposEntidadesInsertar";
 
-	public void Cargar(ref clsBeTbUsuarios oBeTiposEntidades, ref DataRow dr) {
-		try {
-				oBeTiposEntidades.IdTipoEntidad = ( IsDBNull(dr.Item['IdTipoEntidad']) ? 0 : dr.Item['IdTipoEntidad'] )
-				oBeTiposEntidades.Descripcion = ( IsDBNull(dr.Item['Descripcion']) ? '' : dr.Item['Descripcion'] )
-				oBeTiposEntidades.IdGrupoEntidad = ( IsDBNull(dr.Item['IdGrupoEntidad']) ? 0 : dr.Item['IdGrupoEntidad'] )
-				oBeTiposEntidades.Comentario = ( IsDBNull(dr.Item['Comentario']) ? '' : dr.Item['Comentario'] )
-				oBeTiposEntidades.Status = ( IsDBNull(dr.Item['Status']) ? '' : dr.Item['Status'] )
-				oBeTiposEntidades.NoEliminable = ( IsDBNull(dr.Item['NoEliminable']) ? False : dr.Item['NoEliminable'] )
-				oBeTiposEntidades.FechaRegistro = ( IsDBNull(dr.Item['FechaRegistro']) ? Date.Now() : dr.Item['FechaRegistro'] )
-		catch (Exception ex) {
-			throw ex;
-		}
-	}
+            SqlConnection sqlConnection = new SqlConnection(Datos.DBConnection);
+            SqlCommand sqlCommand = new SqlCommand(storedProcedure, sqlConnection);
+            
+            sqlConnection.Open();
 
-	public int Insertar(ref clsBeTiposEntidades oBeTiposEntidades) {
-		try {
-			string sp = 'SpTiposEntidadesInsertar';
-			
-			SqlConnection cnn = new SqlConnection(oConexion.ConexionLocal);
-			SqlClient.SqlCommand cmd = new SqlClient.SqlCommand(sp, cnn);
-			
-			int rowsAffected = 0;
-			cmd.Open();
-			
-			cmd.Parameters.Add(new SqlClient.SqlParameter("@IDTIPOENTIDAD", oBeTiposEntidades.IdTipoEntidad));
-			cmd.Parameters("@IDTIPOENTIDAD").Direction = ParameterDirection.Output;
-			cmd.Parameters.Add(new SqlClient.SqlParameter("@DESCRIPCION", oBeTiposEntidades.Descripcion));
-			cmd.Parameters.Add(new SqlClient.SqlParameter("@IDGRUPOENTIDAD", oBeTiposEntidades.IdGrupoEntidad));
-			cmd.Parameters.Add(new SqlClient.SqlParameter("@COMENTARIO", oBeTiposEntidades.Comentario));
-			cmd.Parameters.Add(new SqlClient.SqlParameter("@STATUS", oBeTiposEntidades.Status));
-			cmd.Parameters.Add(new SqlClient.SqlParameter("@NOELIMINABLE", oBeTiposEntidades.NoEliminable));
-			cmd.Parameters.Add(new SqlClient.SqlParameter("@FECHAREGISTRO", oBeTiposEntidades.FechaRegistro));
-			
-			rowsAffected = cmd.ExecuteNonQuery();
-			oBeTiposEntidades.IdTipoEntidad = int.Parse(cmd.Parameters['@IDTIPOENTIDAD'].Value);
-			
-			return rowsAffected;
-			
-		}
-		catch (Exception ex) {
-			throw ex;
-		}
-	}
+            sqlCommand.Parameters.Add(new SqlParameter("@IDTIPOENTIDAD", oBeTiposEntidades.IdTipoEntidad));
+            sqlCommand.Parameters["@IDTIPOENTIDAD"].Direction = ParameterDirection.Output;
+            sqlCommand.Parameters.Add(new SqlParameter("@DESCRIPCION", oBeTiposEntidades.Descripcion));
+            sqlCommand.Parameters.Add(new SqlParameter("@IDGRUPOENTIDAD", oBeTiposEntidades.IdGrupoEntidad));
+            sqlCommand.Parameters.Add(new SqlParameter("@COMENTARIO", oBeTiposEntidades.Comentario));
+            sqlCommand.Parameters.Add(new SqlParameter("@STATUS", oBeTiposEntidades.Status));
+            sqlCommand.Parameters.Add(new SqlParameter("@NOELIMINABLE", oBeTiposEntidades.NoEliminable));
+            sqlCommand.Parameters.Add(new SqlParameter("@FECHAREGISTRO", oBeTiposEntidades.FechaRegistro));
 
-	public int Actualizar(ref clsBeTiposEntidades oBeTiposEntidades) {
-		try {
-			string sp = 'SpTiposEntidadesActualizar';
-			
-			SqlConnection cnn = new SqlConnection(oConexion.ConexionLocal);
-			SqlClient.SqlCommand cmd = new SqlClient.SqlCommand(sp, cnn);
-			
-			int rowsAffected = 0;
-			cmd.Open();
-			
-			cmd.Parameters.Add(new SqlClient.SqlParameter("@IDTIPOENTIDAD", oBeTiposEntidades.IdTipoEntidad));
-			cmd.Parameters.Add(new SqlClient.SqlParameter("@DESCRIPCION", oBeTiposEntidades.Descripcion));
-			cmd.Parameters.Add(new SqlClient.SqlParameter("@IDGRUPOENTIDAD", oBeTiposEntidades.IdGrupoEntidad));
-			cmd.Parameters.Add(new SqlClient.SqlParameter("@COMENTARIO", oBeTiposEntidades.Comentario));
-			cmd.Parameters.Add(new SqlClient.SqlParameter("@STATUS", oBeTiposEntidades.Status));
-			cmd.Parameters.Add(new SqlClient.SqlParameter("@NOELIMINABLE", oBeTiposEntidades.NoEliminable));
-			cmd.Parameters.Add(new SqlClient.SqlParameter("@FECHAREGISTRO", oBeTiposEntidades.FechaRegistro));
-			
-			rowsAffected = cmd.ExecuteNonQuery();
-			
-			return rowsAffected;
-			
-		}
-		catch (Exception ex) {
-			throw ex;
-		}
-	}
+            rowsAffected = sqlCommand.ExecuteNonQuery();
+            oBeTiposEntidades.IdTipoEntidad = Convert.ToInt32(sqlCommand.Parameters["@IDTIPOENTIDAD"].Value.ToString());
+            
+            return rowsAffected;
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+    }
 
-	public int Eliminar(ref clsBeTiposEntidades oBeTiposEntidades) {
-		try {
-			string sp = 'SpTiposEntidadesEliminar';
-			
-			SqlConnection cnn = new SqlConnection(oConexion.ConexionLocal);
-			SqlClient.SqlCommand cmd = new SqlClient.SqlCommand(sp, cnn);
-			
-			int rowsAffected = 0;
-			cmd.Open();
-			
-			cmd.Parameters.Add(new SqlClient.SqlParameter("@IDTIPOENTIDAD", oBeTiposEntidades.IdTipoEntidad));
-			
-			rowsAffected = cmd.ExecuteNonQuery();
-			
-			return rowsAffected;
-			
-		}
-		catch (Exception ex) {
-			throw ex;
-		}
-	}
+    public int Actualizar(ref clsBeTiposEntidades oBeTiposEntidades)
+    {
+        try
+        {
+            int rowsAffected = 0;
+            string storedProcedure = "SpTiposEntidadesActualizar";
 
-	public DataTable Listar() {
-		try {
-			string sp = 'SpTiposEntidadesListar';
-			
-			SqlConnection cnn = new SqlConnection(oConexion.ConexionLocal);
-			SqlCommand cmd = new SqlCommand(sp, cnn);
-			cmd.CommandType = CommandType.StoredProcedure;
-			
-			SqlDataAdapter dad = new SqlDataAdapter(cmd);
-			
-			DataTable dt = new DataTable();
-			dad.Fill(dt);
-			
-			return dt;
-			
-		}
-		catch (Exception ex) {
-			throw ex;
-		}
-	}
+            SqlConnection sqlConnection = new SqlConnection(Datos.DBConnection);
+            SqlCommand sqlCommand = new SqlCommand(storedProcedure, sqlConnection);
+            
+            sqlConnection.Open();
 
-	public bool Obtener(ref clsBeTiposEntidades oBeTiposEntidades) {
-		try {
-			string sp = 'SpTiposEntidadesObtener';
-			
-			SqlConnection cnn = new SqlConnection(oConexion.ConexionLocal);
-			SqlCommand cmd = new SqlCommand(sp, cnn);
-			cmd.CommandType = CommandType.StoredProcedure;
-			
-			SqlDataAdapter dad = new SqlDataAdapter(cmd);
-			dad.SelectCommand.Parameters.Add(new SqlClient.SqlParameter("@IDTIPOENTIDAD", oBeTiposEntidades.IdTipoEntidad));
-			
-			DataTable dt = new DataTable();
-			dad.Fill(dt);
-			
-			if ((dt.Rows.Count == 1)) {
-				Cargar(oBeTiposEntidades, dt.Rows(0))
-			}
-			else {
-				throw new Exception('No se pudo obtener el registro');
-			}
-			
-			return true;
-			
-		}
-		catch (Exception ex) {
-			throw ex;
-		}
-	}
+            sqlCommand.Parameters.Add(new SqlParameter("@IDTIPOENTIDAD", oBeTiposEntidades.IdTipoEntidad));
+            sqlCommand.Parameters.Add(new SqlParameter("@DESCRIPCION", oBeTiposEntidades.Descripcion));
+            sqlCommand.Parameters.Add(new SqlParameter("@IDGRUPOENTIDAD", oBeTiposEntidades.IdGrupoEntidad));
+            sqlCommand.Parameters.Add(new SqlParameter("@COMENTARIO", oBeTiposEntidades.Comentario));
+            sqlCommand.Parameters.Add(new SqlParameter("@STATUS", oBeTiposEntidades.Status));
+            sqlCommand.Parameters.Add(new SqlParameter("@NOELIMINABLE", oBeTiposEntidades.NoEliminable));
+            sqlCommand.Parameters.Add(new SqlParameter("@FECHAREGISTRO", oBeTiposEntidades.FechaRegistro));
 
-	public bool Primero(ref clsBeTiposEntidades oBeTiposEntidades) {
-		Try
-			string sp = 'SpTiposEntidadesPrimero';
-			
-			SqlConnection cnn = new SqlConnection(oConexion.ConexionLocal);
-			SqlCommand cmd = new SqlCommand(sp, cnn);
-			cmd.CommandType = CommandType.StoredProcedure;
-			
-			SqlDataAdapter dad = new SqlDataAdapter(cmd);
-			
-			DataTable dt = new DataTable();
-			dad.Fill(dt);
-			
-			if ((dt.Rows.Count == 1)) {
-				Cargar(oBeTiposEntidades, dt.Rows[0]);
-			}
-			else {
-				throw new Exception('No se pudo obtener el primer registro');
-			}
-			
-			return true;
-		}
-		catch (Exception ex) {
-			throw ex;
-		}
-	}
+            rowsAffected = sqlCommand.ExecuteNonQuery();
 
-	public bool Primero(ref clsBeTiposEntidades oBeTiposEntidades) {
-		try {
-			string sp = 'SpTiposEntidadesUltimo';
-			
-			SqlConnection cnn = new SqlConnection(oConexion.ConexionLocal);
-			SqlCommand cmd = new SqlCommand(sp, cnn);
-			cmd.CommandType = CommandType.StoredProcedure;
-			
-			SqlDataAdapter dad = new SqlDataAdapter(cmd);
-			
-			DataTable dt = new DataTable();
-			dad.Fill(dt);
-			
-			if ((dt.Rows.Count == 1)) {
-				Cargar(oBeTiposEntidades, dt.Rows[0]);
-			}
-			else {
-				throw new Exception('No se pudo obtener el primer registro');
-			}
-			
-			return true;
-		}
-		catch (Exception ex) {
-			throw ex;
-		}
-	}
+            return rowsAffected;
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+    }
 
-	public bool Anterior(ref clsBeTiposEntidades oBeTiposEntidades) {
-		Try
-			string sp = 'SpTiposEntidadesAnterior';
-			
-			SqlConnection cnn = new SqlConnection(oConexion.ConexionLocal);
-			SqlCommand cmd = new SqlCommand(sp, cnn);
-			cmd.CommandType = CommandType.StoredProcedure;
-			
-			SqlDataAdapter dad = new SqlDataAdapter(cmd);
-			dad.SelectCommand.Parameters.Add(new SqlClient.SqlParameter("@IDTIPOENTIDAD", oBeTiposEntidades.IdTipoEntidad));
-			
-			DataTable dt = new DataTable();
-			dad.Fill(dt);
-			
-			if ((dt.Rows.Count == 1)) {
-				Cargar(oBeTiposEntidades, dt.Rows[0]);
-			}
-			else {
-				throw new Exception('No se pudo obtener el anterior registro');
-			End If
-			}
-			
-			return true;
-		}
-		catch (Exception ex) {
-			throw ex;
-		}
-	}
+    public int Eliminar(ref clsBeTiposEntidades oBeTiposEntidades)
+    {
+        try
+        {
+            int rowsAffected = 0;
+            string storedProcedure = "SpTiposEntidadesEliminar";
 
-	public bool Siguiente(ref clsBeTiposEntidades oBeTiposEntidades) {
-		Try
-			string sp = 'SpTiposEntidadesSiguiente';
-			
-			SqlConnection cnn = new SqlConnection(oConexion.ConexionLocal);
-			SqlCommand cmd = new SqlCommand(sp, cnn);
-			cmd.CommandType = CommandType.StoredProcedure;
-			
-			SqlDataAdapter dad = new SqlDataAdapter(cmd);
-			dad.SelectCommand.Parameters.Add(new SqlClient.SqlParameter("@IDTIPOENTIDAD", oBeTiposEntidades.IdTipoEntidad));
-			
-			DataTable dt = new DataTable();
-			dad.Fill(dt);
-			
-			if ((dt.Rows.Count == 1)) {
-				Cargar(oBeTiposEntidades, dt.Rows[0]);
-			}
-			else {
-				throw new Exception('No se pudo obtener el siguiente registro');
-			End If
-			}
-			
-			return true;
-		}
-		catch (Exception ex) {
-			throw ex;
-		}
-	}
+            SqlConnection sqlConnection = new SqlConnection(Datos.DBConnection);
+            SqlCommand sqlCommand = new SqlCommand(storedProcedure, sqlConnection);
+            
+            sqlConnection.Open();
+            sqlCommand.Parameters.Add(new SqlParameter("@IDTIPOENTIDAD", oBeTiposEntidades.IdTipoEntidad));
+            rowsAffected = sqlCommand.ExecuteNonQuery();
 
+            return rowsAffected;
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+    }
+
+    public DataTable Listar()
+    {
+        try
+        {
+            string storedProcedure = "SpTiposEntidadesListar";
+
+            DataTable dataTable = new DataTable();
+            SqlConnection sqlConnection = new SqlConnection(Datos.DBConnection);
+            
+            SqlCommand sqlCommand = new SqlCommand(storedProcedure, sqlConnection);
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+            sqlDataAdapter.Fill(dataTable);
+
+            return dataTable;
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+    }
+
+    public bool Obtener(ref clsBeTiposEntidades oBeTiposEntidades)
+    {
+        try
+        {
+            string storedProcedure = "SpTiposEntidadesObtener";
+
+            DataTable dataTable = new DataTable();
+            SqlConnection sqlConnection = new SqlConnection(Datos.DBConnection);
+            
+            SqlCommand sqlCommand = new SqlCommand(storedProcedure, sqlConnection);
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+            sqlDataAdapter.SelectCommand.Parameters.Add(new SqlParameter("@IDTIPOENTIDAD", oBeTiposEntidades.IdTipoEntidad));
+            sqlDataAdapter.Fill(dataTable);
+
+            if ((dataTable.Rows.Count == 1))
+            {
+                Cargar(ref oBeTiposEntidades, dataTable.Rows[0]);
+            }
+            else
+            {
+                throw new Exception("No se pudo obtener el registro");
+            }
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+    }
 }
